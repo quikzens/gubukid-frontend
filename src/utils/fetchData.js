923d1d1467fbab4f46e8e2f5114c60473caea3df
Febrian Amir <febrianamir5@gmail.com>
2022-05-30 09:46:31 +0000

    util: update fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,12 +1,13 @@
 import { API } from '../config/api'
 
 export const fetchData = async (url, setData, setLoading, invokeAlert) => {
+  setLoading(true)
+
   try {
     const response = await API.get(url, { withCredentials: true })
     setData(response.data.data)
+    setTimeout(() => setLoading(false), 300)
   } catch (err) {
     invokeAlert('error', `Error Fetch Data From API`)
   }
-
-  setTimeout(() => setLoading(false), 300)
 }
